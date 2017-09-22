ed1bfb55a62dfa8608f7aa5ea0744482441657cf
mwerumuchai <mwerumuchai@gmail.com>
2017-09-22 16:39:35 +0300

    debugg update action

--- app/components/category-tile.js
+++ app/components/category-tile.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  updateCategoryForm: false,
+  actions: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    },
+    imageHide() {
+      this.set('isImageShowing', false);
+    },
+    update(category, params) {
+      this.sendAction('update', category, params);
+    },
+  }
 });
--- app/components/update-category.js
+++ app/components/update-category.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateCategoryForm: false,
+  actions: {
+    updateCategoryForm() {
+      this.set('updateCategoryForm', true);
+    },
+    update(category) {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+      };
+      this.set('updateCategoryForm', false);
+      this.sendAction('update', category, params);
+    }
+  }
 });

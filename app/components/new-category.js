b5946b3dbfcbfaff64dedce0b76534289f4953ec
mwerumuchai <mwerumuchai@gmail.com>
2017-09-22 17:35:29 +0300

    create link to other pages

--- app/components/new-category.js
+++ app/components/new-category.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  addNewCategory: false,
+  actions: {
+    categorylFormShow() {
+      this.set('addNewCategory', true);
+    },
+    saveCategory1() {
+        var params = {
+          question: this.get('question'),
+          author: this.get('author'),
+        };
+        this.set('addNewCategory', false);
+        this.sendAction('saveCategory2', params);
+      }
+    }
+  });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-      return this.store.findAll('category');
+    return this.store.findAll('category');
   },
-  update(category, params) {
-  Object.keys(params).forEach(function(key) {
-    if(params[key]!==undefined) {
-      category.set(key,params[key]);
+  actions: {
+    saveCategory3(params) {
+      var newCategory = this.store.createRecord('category', params);
+      newCategory.save();
+      this.transitionTo('index');
+    },
+    update(category, params) {
+      category.save();
+      this.transitionTo('index');
     }
-  });
-  category.save();
-  this.transitionTo('index');
-},
+  }
 });

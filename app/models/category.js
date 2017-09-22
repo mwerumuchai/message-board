e1989dd8db8bcb34170e2836c4be1a0cc8fd4544
mwerumuchai <mwerumuchai@gmail.com>
2017-09-22 15:44:44 +0300

    review

--- app/models/category.js
+++ app/models/category.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   categoryname: DS.attr(),
-  image: DS.attr()
+  image: DS.attr(),
+  author: DS.attr(),
+  question: DS.attr()
 });

ab5761e661a85b9f7f38e77ec027326fd3068f85
mwerumuchai <mwerumuchai@gmail.com>
2017-09-22 14:33:52 +0300

    adding dynamic segment

--- app/router.js
+++ app/router.js
@@ -3,12 +3,13 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+
 });
 
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('category', {path: '/category/:category_id'});
 });
 
 export default Router;

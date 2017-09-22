b33fa6711c0fbf8b80e28a89fe13912d7fe67e36
mwerumuchai <mwerumuchai@gmail.com>
2017-09-22 16:47:55 +0300

    save button

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   model() {
       return this.store.findAll('category');
   },
+  update(category, params) {
+  Object.keys(params).forEach(function(key) {
+    if(params[key]!==undefined) {
+      category.set(key,params[key]);
+    }
+  });
+  category.save();
+  this.transitionTo('index');
+},
 });

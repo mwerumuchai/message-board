3e160b3622673a3ef2d6c14815183face06cfd6a
mwerumuchai <mwerumuchai@gmail.com>
2017-09-23 23:06:15 +0300

    add navBar

--- app/components/category-tile.js
+++ app/components/category-tile.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isImageShowing: false,
+  
   updateCategoryForm: false,
-  actions: {
-    imageShow() {
-      this.set('isImageShowing', true);
-    },
-    imageHide() {
-      this.set('isImageShowing', false);
-    }
-  }
+
 });

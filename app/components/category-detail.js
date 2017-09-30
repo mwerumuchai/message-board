ffad0ed0074eccf2a256416295590ca0dc100571
mwerumuchai <mwerumuchai@gmail.com>
2017-09-30 21:29:03 +0300

    fix bugs on helpers

--- app/components/category-detail.js
+++ app/components/category-detail.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['rating:desc'],
+  sortedAnswers: Ember.computed.sort('category.answers', 'sortBy'),
+
   actions: {
     destroyAnswer(answer) {
-      this.sendAction('destroyAnswer',answer);
+      this.sendAction('destroyAnswer', answer);
     }
   }
 });

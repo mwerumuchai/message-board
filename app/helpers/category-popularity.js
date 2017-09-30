b82c0425f7f8ffe3438d1b48d5d0fc0a24fbed67
mwerumuchai <mwerumuchai@gmail.com>
2017-10-01 02:05:48 +0300

    fix bug

--- app/helpers/category-popularity.js
+++ app/helpers/category-popularity.js
@@ -7,7 +7,7 @@ export function categoryPopularity(params) {
   if (category.get('answers').get('length') === 0) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star-empty"></span>');
   } else if (category.get('answers').get('length') <= 2) {
-    popularity = Ember.String.htmlSafe('<span class="glyphicon glyphicon-pencil"></span><span>Needs More Answers</span>');
+    popularity = Ember.String.htmlSafe('<span class="glyphicon glyphicon-pencil"></span><span>Let us discuss more</span>');
   } else {
     popularity = Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span><span>Hot Topic!</span>');
   }

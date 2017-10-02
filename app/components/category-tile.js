2652809a9f76b40b3e9870a0409236bdede923b7
mwerumuchai <mwerumuchai@gmail.com>
2017-10-02 09:10:42 +0300

    add service

--- app/components/category-tile.js
+++ app/components/category-tile.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   updateCategoryForm: false,
+  /*favoriteCategory: Ember.inject.service(),
+
+  actions: {
+    addToCategory(favorite) {
+      this.get('favoriteCategory').add(favorite);
+    },
+  }*/
 
 });
---
+++ app/components/show-favorite.js
@@ -0,0 +1,6 @@
+/*import Ember from 'ember';
+
+export default Ember.Component.extend({
+  favoriteCategory: Ember.inject.service(),
+});
+*/
